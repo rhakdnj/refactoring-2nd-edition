eca07410f0b07926a90d17e28829b425177fab13
rhakdnj <chungjm0711@gmail.com>
2023-10-14 23:50:55 +0900

    refactor: 6.2 간접 호출을 너무 과하게 쓰는 코드 인라인 (#6)

--- refacotring-javascript/chapter06/src/02-1.ts
+++ refacotring-javascript/chapter06/src/02-1.ts
@@ -1,5 +1,4 @@
-const moreThanFiveLateDeliveries = (driver: any): boolean => driver.numberOfLateDeliveries > 5;
-const rating = (driver: any): number => (moreThanFiveLateDeliveries(driver) ? 2 : 1);
+const rating = (driver: any): number => (driver.numberOfLateDeliveries > 5 ? 2 : 1);
 
 const driverA = {name: 'A', numberOfLateDeliveries: 10};
 const driverB = {name: 'B', numberOfLateDeliveries: 4};
