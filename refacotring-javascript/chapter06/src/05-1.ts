e9c3bf93eca5b7ac5afd3f2f050ea9ec6ccfc5cd
rhakdnj <chungjm0711@gmail.com>
2023-10-15 00:43:35 +0900

    feat: 6.5.1 함수 이름을 바꿀 때 migration 과정으로 다른 함수가 call하는 방식으로 (#6)

--- refacotring-javascript/chapter06/src/05-1.ts
+++ refacotring-javascript/chapter06/src/05-1.ts
@@ -1,16 +1,18 @@
+import assert from 'assert';
+
 class Book {
     private readonly _reservations: any[] = [];
 
     get reservation() {
         return this._reservations;
     }
-
-    public addReservation(customer: { name: string }): void {
+    public addReservation(customer: any, isPriority: boolean): void {
+        assert(typeof isPriority === 'boolean'); // ts guaranteed
         this._reservations.push(customer);
     }
 }
 
 const bookCafe = new Book();
-bookCafe.addReservation({name: 'joy'});
-bookCafe.addReservation({name: 'jay'});
+bookCafe.addReservation({name: 'joy'}, true);
+bookCafe.addReservation({name: 'jay'}, false);
 console.log(bookCafe.reservation);
