89b524b49500579253ba8f1dca8d4ec0837d0852
rhakdnj <chungjm0711@gmail.com>
2023-10-15 19:40:47 +0900

    refactor: 6.11-2 단계 쪼개기 (#6)

--- refacotring-javascript/chapter06/src/11-2.ts
+++ refacotring-javascript/chapter06/src/11-2.ts
@@ -17,22 +17,46 @@ class Order {
     }
 }
 
-const main = () => {
-    try {
-        const argv: string[] = process.argv;
-        if (argv.length < 3) {
+class CommandLine {
+    private readonly _filename: string;
+    private readonly _onlyCountReady: boolean;
+
+    constructor(args: string[]) {
+        if (args.length < 3) {
             throw new Error('파일명을 입력하세요');
         }
-        const filename = argv[argv.length - 1];
-        const input = readJSON(filename);
-        const orders = input.map((item: any) => new Order(item));
+        this._filename = args[args.length - 1];
+        this._onlyCountReady = args.includes('-r');
+    }
 
-        if (argv.includes('-r')) {
-            const readyOrders = orders.filter((o: any) => o.product.status === 'ready');
-            console.log('ready', readyOrders.length);
-        } else {
-            console.log('not ready', orders.length);
-        }
+    get filename(): string {
+        return this._filename;
+    }
+
+    get onlyCountReady(): boolean {
+        return this._onlyCountReady;
+    }
+}
+
+const run = (args: string[]) => {
+    return countOrders(new CommandLine(args));
+};
+
+const countOrders = (commandLine: CommandLine) => {
+    const input = readJSON(commandLine.filename);
+    const orders = input.map((item: any) => new Order(item));
+
+    if (commandLine.onlyCountReady) {
+        const readyOrders = orders.filter((o: any) => o.product.status === 'ready');
+        return `ready: ${readyOrders.length}`;
+    } else {
+        return `not ready: ${orders.length}`;
+    }
+};
+
+const main = () => {
+    try {
+        console.log(run(process.argv));
     } catch (err) {
         console.error(err);
     }
