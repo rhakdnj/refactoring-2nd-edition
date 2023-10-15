633794d495c3219a3f56bfbc6d1388175e7fcdff
rhakdnj <chungjm0711@gmail.com>
2023-10-15 15:57:17 +0900

    feat: 6.10 여러 함수를 변환 함수로 묶기 (#6)

---
+++ refacotring-javascript/chapter06/src/10.ts
@@ -0,0 +1,30 @@
+const acquireReading = (): any => ({
+    customer: 'ivan',
+    quantity: 10,
+    month: 5,
+    year: 2017,
+});
+const baseRate = (month: number, year: number) => year - 2000 + month;
+
+const client1 = () => {
+    const reading: any = acquireReading();
+    const baseCharge = baseRate(reading.month, reading.year) * reading.quantity;
+    return baseCharge;
+};
+
+const client2 = () => {
+    const taxThreshold = (year: number) => (year - 2000) * 0.1;
+    const reading: any = acquireReading();
+    const base: number = baseRate(reading.month, reading.year) * reading.quantity;
+    const taxableCharge: number = Math.max(0, base - taxThreshold(reading.year));
+    return taxableCharge;
+};
+
+const client3 = () => {
+    const reading = acquireReading();
+    const calculateBaseCharge = (reading: any) => baseRate(reading.month, reading.year) * reading.quantity;
+    const basicChargeAmount = calculateBaseCharge(reading);
+    return basicChargeAmount;
+};
+
+[client1, client2, client3].forEach(c => console.log(c()));
