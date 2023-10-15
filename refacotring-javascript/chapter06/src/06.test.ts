7718e9cfd63192df91aa5c99600b4b27934ef91d
rhakdnj <chungjm0711@gmail.com>
2023-10-15 14:31:14 +0900

    refactor: 06-6.1 setter 허용 하지 않고 불변 객체로 만들자 (#6)

--- refacotring-javascript/chapter06/src/06.test.ts
+++ refacotring-javascript/chapter06/src/06.test.ts
@@ -1,6 +1,6 @@
 let defaultOwnerData: any = {firstName: '마틴', lastName: '파울러'};
 
-const defaultOwner = () => defaultOwnerData;
+const defaultOwner = () => ({...defaultOwnerData}); // 얉은 복사
 const setDefaultOwner = (otherOwnerData: any) => {
     defaultOwnerData = otherOwnerData;
 };
@@ -11,5 +11,25 @@ it('test', () => {
 
     const owner2 = defaultOwner();
     owner2.lastName = '파슨스';
-    expect(owner1.lastName).toBe('파슨스');
+    expect(owner1.lastName).toBe('파울러');
 });
+
+const defaultPerson = () => new Person(defaultOwnerData);
+
+class Person {
+    private readonly _firstName: string;
+    private readonly _lastName: string;
+
+    public constructor(data: any) {
+        this._firstName = data.firstName;
+        this._lastName = data.lastName;
+    }
+
+    public get firstName(): string {
+        return this._firstName;
+    }
+
+    public get lastName(): string {
+        return this._lastName;
+    }
+}
