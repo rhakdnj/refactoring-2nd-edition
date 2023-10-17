e9692f88c3d09385a3da9b234336cc5d5525e028
rhakdnj <chungjm0711@gmail.com>
2023-10-18 00:00:47 +0900

    feat: 7.1-2 클래스를 정의 및 반환 함수 추가 (#7)

--- refacotring-javascript/chapter07/src/01-2.test.ts
+++ refacotring-javascript/chapter07/src/01-2.test.ts
@@ -1,14 +1,13 @@
-import { compareUsage, getCustomer, writeData } from './01-2';
-import { IllegalStateException } from '@js-joda/core';
+import { compareUsage, getRawDataOfCustomers, writeData } from './01-2';
 
 describe('customerData', () => {
     it('initial usage of 1920 at 2016/1 to be 50', () => {
-        expect(getCustomer()['1920'].usages['2016']['1']).toBe(50);
+        expect(getRawDataOfCustomers()['1920'].usages['2016']['1']).toBe(50);
     })
 
     it('writeData', () => {
         writeData('1920', '2016', parseInt('1'), 53)
-        expect(getCustomer()['1920'].usages['2016']['1']).toBe(53)
+        expect(getRawDataOfCustomers()['1920'].usages['2016']['1']).toBe(53)
     })
 
     it('compareUsage', () => {
--- refacotring-javascript/chapter07/src/01-2.ts
+++ refacotring-javascript/chapter07/src/01-2.ts
@@ -1,18 +1,29 @@
 import { readJSON } from '../../file.controller';
 import * as path from 'path';
 
-let customerData = readJSON(path.resolve(__dirname, '01-2.json'));
+class CustomerData {
+    _data;
 
-const getRawDataOfCustomers = () => customerData;
+    constructor(data: any) {
+        this._data = data;
+    }
+}
+
+
+let customerData = new CustomerData(readJSON(path.resolve(__dirname, '01-2.json')));
+
+const getCustomerData = () => customerData;
+export const getRawDataOfCustomers = () => customerData._data;
+const setRawDataOfCustomers = (data: any) => {
+    customerData = new CustomerData(data);
+};
 
 export const writeData = (customerId: string, year: string, month: number, amount: number) => {
     getRawDataOfCustomers()[customerId].usages[year][month] = amount;
 };
 
 export const compareUsage = (customerId: string, laterYear: number, month: number) => {
-    const later = customerData[customerId].usages[laterYear][month];
-    const earlier = customerData[customerId].usages[laterYear - 1][month];
+    const later = getRawDataOfCustomers()[customerId].usages[laterYear][month];
+    const earlier = getRawDataOfCustomers()[customerId].usages[laterYear - 1][month];
     return {laterAmount: later, change: later - earlier};
 };
-
-export const getCustomer = () => customerData;
