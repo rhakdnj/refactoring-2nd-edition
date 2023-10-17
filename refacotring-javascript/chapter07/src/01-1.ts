df684fac042dadc514fd1ef28441bcd393d446ff
rhakdnj <chungjm0711@gmail.com>
2023-10-17 23:03:00 +0900

    feat: 7.1 레코드를 클래스로, 새 클래스의 인스턴스를 반환하는 함수를 추가 (#7)

--- refacotring-javascript/chapter07/src/01-1.ts
+++ refacotring-javascript/chapter07/src/01-1.ts
@@ -1,7 +1,16 @@
 let result = '';
-const organization = {name: '애크미 구스베리', country: 'GB'};
+class Organization {
+    _data;
+    constructor(data: any) {
+        this._data = data;
+    }
+}
+const organization = new Organization({name: '애크미 구스베리', country: 'GB'});
 
 const getRawDataOfOrganization = () => {
+    return organization._data;
+}
+const getOrganization = () => {
     return organization;
 }
 
