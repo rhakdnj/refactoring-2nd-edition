5fe7b3d49e26518f741cc648cd3c445fb127bcc3
rhakdnj <chungjm0711@gmail.com>
2023-10-18 00:24:43 +0900

    feat: 7.1-2 rawData 반환 및 setUsage 함수 추가 (#7)

--- refacotring-javascript/chapter07/src/01-2.ts
+++ refacotring-javascript/chapter07/src/01-2.ts
@@ -1,11 +1,20 @@
 import { readJSON } from '../../file.controller';
 import * as path from 'path';
+import { cloneDeep } from 'lodash';
 
 class CustomerData {
     _data;
 
     constructor(data: any) {
         this._data = data;
+    }
+
+    setUsage = (customerId: string, year: string, month: number, amount: number) => {
+        getRawDataOfCustomers()[customerId].usages[year][month] = amount;
+    }
+
+    get rawData() {
+        return cloneDeep(this._data);
     }
 }
 
@@ -19,7 +28,7 @@ const setRawDataOfCustomers = (data: any) => {
 };
 
 export const writeData = (customerId: string, year: string, month: number, amount: number) => {
-    getRawDataOfCustomers()[customerId].usages[year][month] = amount;
+    getCustomerData().setUsage(customerId, year, month, amount);
 };
 
 export const compareUsage = (customerId: string, laterYear: number, month: number) => {
