8ad667bdcfc6c0c3d2ffd993e7a0f1c9f44ac651
rhakdnj <chungjm0711@gmail.com>
2023-10-17 23:05:58 +0900

    feat: 7.1 레코드를 갱신은 세터를 사용하도록, 레코드를 읽는 것은 게터를 사용한다 (#7)

--- refacotring-javascript/chapter07/src/01-1.ts
+++ refacotring-javascript/chapter07/src/01-1.ts
@@ -4,6 +4,14 @@ class Organization {
     constructor(data: any) {
         this._data = data;
     }
+
+    set name(name: string) {
+        this._data.name = name;
+    }
+
+    get name() {
+        return this._data.name;
+    }
 }
 const organization = new Organization({name: '애크미 구스베리', country: 'GB'});
 
@@ -14,7 +22,7 @@ const getOrganization = () => {
     return organization;
 }
 
-getRawDataOfOrganization().name = '애그머니 블루베리';
-result += `<h1>${getRawDataOfOrganization().name}</h1>`;
+getOrganization().name = '애그머니 블루베리';
+result += `<h1>${getOrganization().name}</h1>`;
 
 console.log(result);
