fbff318e39ea19e06f1dda9e72a1c834ce909059
rhakdnj <chungjm0711@gmail.com>
2023-10-17 23:07:53 +0900

    feat: 7.1 임시 함수를 제거한다 (#7)

--- refacotring-javascript/chapter07/src/01-1.ts
+++ refacotring-javascript/chapter07/src/01-1.ts
@@ -15,9 +15,6 @@ class Organization {
 }
 const organization = new Organization({name: '애크미 구스베리', country: 'GB'});
 
-const getRawDataOfOrganization = () => {
-    return organization._data;
-}
 const getOrganization = () => {
     return organization;
 }
