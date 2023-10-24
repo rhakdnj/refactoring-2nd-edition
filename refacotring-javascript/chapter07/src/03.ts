551f6eb5f854af2a19255a4798688c0f0798cb15
rhakdnj <chungjm0711@gmail.com>
2023-10-24 21:58:20 +0900

    refactor: 7.3 기본형을 객체로 바꾸기 (#7)

--- refacotring-javascript/chapter07/src/03.ts
+++ refacotring-javascript/chapter07/src/03.ts
@@ -1,12 +1,54 @@
 class Order {
-    private readonly _priority = '';
+    #priority: Priority;
 
     constructor(data: any) {
-        this._priority = data.priority;
+        this.#priority = data.priority;
     }
 
-    get priority(): string {
-        return this._priority;
+    get priorityString(): string {
+        return this.#priority.toString();
+    }
+
+    get priority(): Priority {
+        return this.#priority;
+    }
+
+    set priority(value: string) {
+        this.#priority = new Priority(value);
+    }
+}
+
+type PriorityType = 'low' | 'normal' | 'high' | 'rush';
+
+class Priority {
+    static legalValues(): PriorityType[] {
+        return ['low', 'normal', 'high', 'rush'];
+    }
+
+    #value: string;
+
+    constructor(value: string) {
+        this.#value = value;
+    }
+
+    toString(): string {
+        return this.#value;
+    }
+
+    public get index() {
+        return Priority.legalValues().findIndex((s: PriorityType) => s === this.#value);
+    }
+
+    public equals(other: Priority): boolean {
+        return this.index === other.index;
+    }
+
+    public higherThan(other: Priority): boolean {
+        return this.index > other.index;
+    }
+
+    public lowerThan(other: Priority) {
+        return this.index < other.index;
     }
 }
 
@@ -14,7 +56,7 @@ const client1 = () => {
     const orders: any[] = [{priority: 'high'}, {priority: 'rush'}, {priority: 'low'}, {priority: 'normal'}].map(
         o => new Order(o),
     );
-    const highPriorityCount = orders.filter(o => o.priority === 'high' || o.priority === 'rush').length;
+    const highPriorityCount = orders.filter(o => o.priorityString === 'high' || o.priorityString === 'rush').length;
     return highPriorityCount;
 };
 console.log(client1());
