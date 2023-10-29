21ab32fdab7584f1439d1c2977a73965b2863e24
rhakdnj <chungjm0711@gmail.com>
2023-10-29 11:14:43 +0900

    refactor: 7.4 임시 변수를 질의 함수로 바꾸기 (#7)

--- refacotring-javascript/chapter07/src/04.ts
+++ refacotring-javascript/chapter07/src/04.ts
@@ -8,10 +8,19 @@ class Order {
     }
 
     get price() {
-        let basePrice: number = this._quantity * this._item.price;
+        return this.basePrice * this.discountFactor;
+    }
+
+    private get basePrice(): number {
+        return this._quantity * this._item.price;
+    }
+
+    private get discountFactor(): number {
         let discountFactor: number = 0.98;
-        if (basePrice > 1000) discountFactor -= 0.03;
-        return basePrice * discountFactor;
+        if (this.basePrice > 1000) {
+            discountFactor -= 0.03;
+        }
+        return discountFactor;
     }
 }
 
