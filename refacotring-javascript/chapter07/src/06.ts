d032e111489e9424be3de4ca592177f640361fef
rhakdnj <chungjm0711@gmail.com>
2023-10-29 12:29:19 +0900

    refactor: 7.6 클래스 인라인하기  (#7)

--- refacotring-javascript/chapter07/src/06.ts
+++ refacotring-javascript/chapter07/src/06.ts
@@ -1,53 +1,25 @@
-class TrackingInformation {
+class Shipment {
     private _shippingCompany: string | undefined; // 배송 회사
     private _trackingNumber: string | undefined; // 추적 번호
 
-    get shippingCompany() {
-        return this._shippingCompany;
+    get trackingInfo() {
+        return `${this._shippingCompany}: ${this._trackingNumber}`;
     }
 
-    set shippingCompany(arg) {
+    set shippingCompany(arg: string) {
         this._shippingCompany = arg;
     }
 
-    get trackingNumber() {
-        return this._trackingNumber;
-    }
-
-    set trackingNumber(arg) {
+    set trackingNumber(arg: string) {
         this._trackingNumber = arg;
-    }
-
-    get display() {
-        return `${this.shippingCompany}: ${this.trackingNumber}`;
-    }
-}
-
-class Shipment {
-    private _trackingInformation: TrackingInformation;
-
-    constructor() {
-        this._trackingInformation = new TrackingInformation();
-    }
-
-    get trackingInfo() {
-        return this._trackingInformation.display;
-    }
-
-    get trackingInformation() {
-        return this._trackingInformation;
-    }
-
-    set trackingInformation(aTrackingInformation) {
-        this._trackingInformation = aTrackingInformation;
     }
 }
 
 const client1 = () => {
     const aShipment = new Shipment();
     const request: any = {vendor: 'A-SHIP', number: '010-1234-5678'};
-    aShipment.trackingInformation.shippingCompany = request.vendor;
-    aShipment.trackingInformation.trackingNumber = request.number;
+    aShipment.shippingCompany = request.vendor;
+    aShipment.trackingNumber = request.number;
     return aShipment.trackingInfo;
 };
 
