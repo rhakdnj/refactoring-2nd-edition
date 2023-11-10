8da96cf31b000456188c5895560cfb147c6b64c9
rhakdnj <chungjm0711@gmail.com>
2023-11-10 23:18:53 +0900

    feat: 05 init (#9)

---
+++ refacotring-javascript/chapter09/src/05.ts
@@ -0,0 +1,28 @@
+class Customer {
+    #id;
+
+    constructor(id: string) {
+        this.#id = id;
+    }
+
+    get id() {
+        return this.#id;
+    }
+}
+
+class Order {
+    #number: number;
+    #customer: Customer;
+
+    constructor(data: any) {
+        this.#number = data.number;
+        this.#customer = new Customer(data.customer);
+    }
+
+    get customer() {
+        return this.#customer;
+    }
+}
+
+const order: Order = new Order({number: 1, customer: 'a'});
+console.log(order.customer.id);
