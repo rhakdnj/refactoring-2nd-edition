eb5fdf1c725e1637f356c7f2f88ccf9ebc170add
rhakdnj <chungjm0711@gmail.com>
2023-11-10 21:46:54 +0900

    refactor: 08 (#8)

--- refacotring-javascript/chapter08/src/08.ts
+++ refacotring-javascript/chapter08/src/08.ts
@@ -7,20 +7,12 @@ Chennai, India, +91 44 660 44766`;
 
 const acquireData = (input: string) => {
     const lines = input.split('\n');
-    let firstLine = true;
-    const result = [];
-    for (const line of lines) {
-        if (firstLine) {
-            firstLine = false;
-            continue;
-        }
-        if (line.trim() === '') continue;
-        const record = line.split(',');
-        if (record[1].trim() === 'India') {
-            result.push({city: record[0].trim(), phone: record[2].trim()});
-        }
-    }
-    return result;
+    return lines
+        .slice(1)
+        .filter((line: string) => line.trim() !== '')
+        .map((line: string) => line.split(','))
+        .filter((record: string[]) => record[1].trim() === 'India')
+        .map((record: string[]) => ({city: record[0].trim(), phone: record[2].trim()}));
 };
 
 console.log(acquireData(csvData));
