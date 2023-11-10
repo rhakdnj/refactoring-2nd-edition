3be25516d4d89b7625cc5d94f2d090f781ffbca8
rhakdnj <chungjm0711@gmail.com>
2023-11-11 00:22:44 +0900

    refactor: 01 (#10)

--- refacotring-javascript/chapter10/src/01.ts
+++ refacotring-javascript/chapter10/src/01.ts
@@ -8,15 +8,14 @@ const plan = {
     regularServiceCharge: 100,
 };
 
-const getCharge = (quantity: number, date: LocalDate) => {
-    let charge;
-    if (!date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd)) {
-        charge = quantity * plan.summerRate;
-    } else {
-        charge = quantity * plan.regularRate + plan.regularServiceCharge;
-    }
-    return charge;
-}
+const isSummer = (date: LocalDate) => !date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd);
+
+const summerCharge = (quantity: number) => quantity * plan.summerRate;
+const regularCharge = (quantity: number) => quantity * plan.regularRate + plan.regularServiceCharge;
+
+const getCharge = (quantity: number, date: LocalDate) =>
+    isSummer(date) ? summerCharge(quantity) : regularCharge(quantity);
+
 
 console.log(getCharge(10, LocalDate.of(2023, 6, 29)));
 console.log(getCharge(10, LocalDate.of(2023, 8, 15)));
