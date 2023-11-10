3eb352a0bf6e0cc51c5e736f3e8900a558e3c243
rhakdnj <chungjm0711@gmail.com>
2023-11-10 22:17:23 +0900

    refactor: 01-1 (#9)

--- refacotring-javascript/chapter09/src/01-1.ts
+++ refacotring-javascript/chapter09/src/01-1.ts
@@ -5,19 +5,30 @@ const scenario = {
     delay: 40,
 };
 
-const distanceTravelled = (scenario: any, time: number) => {
-    let result;
-    let acc: number = scenario.primaryForce / scenario.mass; // (a = F / m)
-    let primaryTime: number = Math.min(time, scenario.delay);
-    result = 0.5 * acc * primaryTime ** 2; // 전파된 거리
-    let secondaryTime = time - scenario.delay;
+const distanceByAcceleration = (acc: number, time: number) =>  0.5 * acc * time ** 2;
 
-    if (secondaryTime > 0) { // 두 번째 힘을 반영해 다시 계산
-        let primaryVelocity = acc * scenario.delay;
-        acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
-        result += primaryVelocity * secondaryTime + 0.5 * acc * secondaryTime ** 2;
-    }
-    return result;
+const primary = (primaryForce: any, mass: any, delay: any, time: any) => {
+    const primaryAcceleration: number = primaryForce / mass; // (a = F / m)
+    const primaryTime: number = Math.min(time, delay);
+    return {primaryAcceleration, primaryDistance: distanceByAcceleration(primaryAcceleration, primaryTime)};
+};
+
+const distanceTravelled = (scenario: any, time: number) => {
+    const {primaryForce, secondaryForce, mass, delay} = scenario;
+
+    let {primaryAcceleration, primaryDistance: primaryResult} = primary(primaryForce, mass, delay, time);
+    const secondaryTime = time - delay;
+
+    if (secondaryTime <= 0) return primaryResult;
+
+    // 두 번째 힘을 반영해 다시 계산
+    let primaryVelocity = primaryAcceleration * delay;
+    const secondaryAcceleration = (primaryForce + secondaryForce) / mass;
+    return (
+        primaryResult +
+        primaryVelocity * secondaryTime +
+        distanceByAcceleration(secondaryAcceleration, secondaryTime)
+    );
 };
 
 console.log(distanceTravelled(scenario, 100));
