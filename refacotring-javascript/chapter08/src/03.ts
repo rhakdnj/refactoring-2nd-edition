25a45d6addb3b5a32057db890d8f5142071cf166
rhakdnj <chungjm0711@gmail.com>
2023-11-10 20:20:34 +0900

    refactor: 03 (#8)

--- refacotring-javascript/chapter08/src/03.ts
+++ refacotring-javascript/chapter08/src/03.ts
@@ -12,27 +12,27 @@ interface Person {
     photo: Photo;
 }
 
-const renderPhoto = (photo: Photo) => {
-    return `<img src="${photo.url}" />`;
-};
-const emitPhotoData = (photo: Photo) => {
-    const result = [];
-    result.push(`<p>위치: ${photo.location}</p>`);
-    result.push(`<p>날짜: ${photo.date}</p>`);
-    return result.join('\n');
-};
+const emitPhotoData = (photo: Photo) =>
+    [
+        `<p>제목: ${photo.title}</p>`,
+        `<p>위치: ${photo.location}</p>`,
+        `<p>날짜: ${photo.date}</p>`,
+    ].join('\n');
 
-const renderPerson = (person: Person) => {
-    const result = [];
-    result.push(`<p>${person.name}</p>`);
-    result.push(renderPhoto(person.photo));
-    result.push(`<p>제목: ${person.photo.title}</p>`);
-    result.push(emitPhotoData(person.photo));
-    return result.join('\n');
+const renderPerson = (person: Person) =>
+    [
+        `<p>${person.name}</p>`,
+        `<img src="${person.photo.url}" />`,
+        emitPhotoData(person.photo)
+    ].join('\n');
+
+const photoDiv = (p: Photo) => ['<div>', emitPhotoData(p), '</div>'].join('\n');
+
+const photo = {
+    title: 'Title',
+    location: '고속터미널',
+    date: LocalDateTime.now(),
+    url: 'http://abc.com'
 };
-
-const photoDiv = (p: Photo) => ['<div>', `<p>제목: ${p.title}</p>`, emitPhotoData(p), '</div>'].join('\n');
-
-const photo = {title: 'Title', location: '고속터미널', date: LocalDateTime.now(), url: 'http://abc.com'};
 console.log('** renderPerson **\n', renderPerson({name: '제이미', photo}));
 console.log('\n** photoDiv **\n', photoDiv(photo));
