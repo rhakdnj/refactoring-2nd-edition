ef64f82f6e2c6d5e3ab2c682a5efe41371352575
rhakdnj <chungjm0711@gmail.com>
2023-11-11 09:58:06 +0900

    refactor: 03-2 조건식을 하나씩 뒤집기 (#10)

--- refacotring-javascript/chapter10/src/03-2.ts
+++ refacotring-javascript/chapter10/src/03-2.ts
@@ -1,9 +1,4 @@
 const adjustCapital = (instrument: any) => {
-    let result;
-    if (instrument.capital > 0) {
-        if (instrument.interestRate > 0 && instrument.duration > 0) {
-            result = (instrument.income / instrument.duration) * instrument.adjustmentFactor;
-        }
-    }
-    return result;
+    return (instrument.capital <= 0 || instrument.interestRate <= 0 || instrument.duration <= 0) ? 0
+        : (instrument.income / instrument.duration) * instrument.adjustmentFactor;
 };
