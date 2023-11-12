a2f998c3c6be8ccf1981ba0485328d6867e718e8
rhakdnj <chungjm0711@gmail.com>
2023-11-12 23:56:12 +0900

    feat: 09 init (#11)

---
+++ refacotring-javascript/chapter11/src/09.ts
@@ -0,0 +1,22 @@
+const score = (candidate: any, medicalExam: any, scoringGuide: any) => {
+    let result = 0;
+    let healthLevel = 0;
+    let highMedicalRiskFlag = false;
+    if (medicalExam.isSmoker) {
+        healthLevel += 10;
+        highMedicalRiskFlag = true;
+    }
+    let certificationGrade = 'regular';
+    if (scoringGuide.stateWithLowCertification(candidate.originState)) {
+        certificationGrade = 'low';
+        result -= 5;
+    }
+    result -= Math.max(healthLevel - 5, 0);
+    return {result, certificationGrade, highMedicalRiskFlag};
+};
+
+const scoringGuide = {
+    stateWithLowCertification: (state: string) => state === 'CA' || state === 'ME'
+};
+console.log(score({originState: 'CA'}, {isSmoker: true}, scoringGuide));
+console.log(score({originState: 'NY'}, {isSmoker: false}, scoringGuide));
