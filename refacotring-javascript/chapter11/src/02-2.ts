2c24da7b959962b32575c07e9a1e5fa8dec04631
rhakdnj <chungjm0711@gmail.com>
2023-11-12 10:26:10 +0900

    refactor: 02-2 (#11)

--- refacotring-javascript/chapter11/src/02-2.ts
+++ refacotring-javascript/chapter11/src/02-2.ts
@@ -5,12 +5,25 @@ const usd = (number: number) =>
         minimumFractionDigits: 2,
     }).format(number / 100);
 
+/**
+ * no use
+ */
 const bottomBand = (usage: number) => Math.min(usage, 100);
 const middleBand = (usage: number) => (usage > 100 ? Math.min(usage, 200) - 100 : 0);
 const topBand = (usage: number) => (usage > 200 ? usage - 200 : 0);
 
+
+const withinBand = (usage: number, bottom: number, top: number) => usage > bottom ?
+    Math.min(usage, top) - bottom : 0;
+
 const baseCharge = (usage: number) => {
+    // withinBand 도입: 보호 구문 제거 가능
     if (usage < 0) return usd(0);
-    const amount = bottomBand(usage) * 0.03 + middleBand(usage) * 0.05 + topBand(usage) * 0.07;
+
+    const amount =
+        withinBand(usage, 0, 100) * 0.03 +
+        withinBand(usage, 100, 200) * 0.05 +
+        withinBand(usage, 200, Infinity) * 0.07;
+
     return usd(amount);
 };
