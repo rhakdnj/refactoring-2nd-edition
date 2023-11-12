348941af13451fbf641403b56b0d4f5bb7c22ce6
rhakdnj <chungjm0711@gmail.com>
2023-11-12 12:31:35 +0900

    refactor: 03-1 (#11)

--- refacotring-javascript/chapter11/src/03-1.ts
+++ refacotring-javascript/chapter11/src/03-1.ts
@@ -3,16 +3,15 @@ import { LocalDate, LocalDateTime } from '@js-joda/core';
 class Place {
     plusDays(time: number) {
         const dateTime = LocalDateTime.parse('2021-07-08T10:00:00.000');
-        dateTime.plusHours(dateTime.hour() + time);
-        return dateTime;
+        return dateTime.plusHours(dateTime.hour() + time);
     }
 }
 
 type DeliveryState = 'MA' | 'NH' | 'CT' | 'ME';
 
 class Order {
-    private _deliveryState: DeliveryState;
-    private _placedOn;
+    private readonly _deliveryState: DeliveryState;
+    private readonly _placedOn: Place;
 
     constructor(deliveryState: DeliveryState) {
         this._deliveryState = deliveryState;
@@ -28,23 +27,23 @@ class Order {
     }
 }
 
-const deliveryDate = (order: Order, isRush: boolean) => {
-    if (isRush) {
-        let deliveryTime;
-        if (['MA', 'CT'].includes(order.deliveryState)) deliveryTime = 1;
-        else if (['NY', 'NH'].includes(order.deliveryState)) deliveryTime = 2;
-        else deliveryTime = 3;
-        return order.placedOn.plusDays(1 + deliveryTime);
-    } else {
-        let deliveryTime;
-        if (['MA', 'CT', 'NY'].includes(order.deliveryState)) deliveryTime = 2;
-        else if (['ME', 'NH'].includes(order.deliveryState)) deliveryTime = 3;
-        else deliveryTime = 4;
-        return order.placedOn.plusDays(2 + deliveryTime);
-    }
-};
+const rushDeliveryDate = (order: Order) => {
+    let deliveryTime;
+    if (['MA', 'CT'].includes(order.deliveryState)) deliveryTime = 1;
+    else if (['NY', 'NH'].includes(order.deliveryState)) deliveryTime = 2;
+    else deliveryTime = 3;
+    return order.placedOn.plusDays(1 + deliveryTime);
+}
 
-console.log(deliveryDate(new Order('MA'), true));
-console.log(deliveryDate(new Order('NH'), true));
-console.log(deliveryDate(new Order('CT'), false));
-console.log(deliveryDate(new Order('ME'), false));
+const regularDeliveryDate = (order: Order) => {
+    let deliveryTime;
+    if (['MA', 'CT', 'NY'].includes(order.deliveryState)) deliveryTime = 2;
+    else if (['ME', 'NH'].includes(order.deliveryState)) deliveryTime = 3;
+    else deliveryTime = 4;
+    return order.placedOn.plusDays(2 + deliveryTime);
+}
+
+console.log(rushDeliveryDate(new Order('MA')).toString());
+console.log(rushDeliveryDate(new Order('NH')).toString());
+console.log(regularDeliveryDate(new Order('CT')).toString());
+console.log(regularDeliveryDate(new Order('ME')).toString());
