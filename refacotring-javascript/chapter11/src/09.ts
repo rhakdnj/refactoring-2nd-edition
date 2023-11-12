76a3a6c0419e214b9e2782b3acf6fab1d01ecf74
rhakdnj <chungjm0711@gmail.com>
2023-11-13 00:06:09 +0900

    refactor: 09 (#11)

--- refacotring-javascript/chapter11/src/09.ts
+++ refacotring-javascript/chapter11/src/09.ts
@@ -1,19 +1,55 @@
 const score = (candidate: any, medicalExam: any, scoringGuide: any) => {
-    let result = 0;
-    let healthLevel = 0;
-    let highMedicalRiskFlag = false;
-    if (medicalExam.isSmoker) {
-        healthLevel += 10;
-        highMedicalRiskFlag = true;
-    }
-    let certificationGrade = 'regular';
-    if (scoringGuide.stateWithLowCertification(candidate.originState)) {
-        certificationGrade = 'low';
-        result -= 5;
-    }
-    result -= Math.max(healthLevel - 5, 0);
-    return {result, certificationGrade, highMedicalRiskFlag};
+    return new Score(candidate, medicalExam, scoringGuide).execute();
 };
+
+class Score {
+    private _result: number;
+    private _healthLevel: number;
+    private _highMedicalRiskFlag: boolean;
+
+    constructor(
+        private readonly _candidate: any,
+        private readonly _medicalExam: any,
+        private readonly _scoringGuide: any
+    ) {
+        this._result = 0;
+        this._healthLevel = 0;
+        this._highMedicalRiskFlag = false;
+    }
+
+    execute() {
+        this._result = 0;
+        this._healthLevel = 0;
+        this._highMedicalRiskFlag = false;
+
+        this.scoreSmoking();
+        const certificationGrade = this.certificationGrade()
+        this._result -= Math.max(this._healthLevel - 5, 0);
+
+        return {
+            result: this._result,
+            certificationGrade,
+            _highMedicalRiskFlag: this._highMedicalRiskFlag
+        };
+    }
+
+    private certificationGrade() {
+        let certificationGrade = 'regular';
+        if (scoringGuide.stateWithLowCertification(this._candidate.originState)) {
+            certificationGrade = 'low';
+            this._result -= 5;
+        }
+        return certificationGrade;
+    }
+
+    private scoreSmoking() {
+        if (this._medicalExam.isSmoker) {
+            this._healthLevel += 10;
+            this._highMedicalRiskFlag = true;
+        }
+    }
+}
+
 
 const scoringGuide = {
     stateWithLowCertification: (state: string) => state === 'CA' || state === 'ME'
