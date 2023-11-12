dc100f624318c9e381f60bf9cf430186f0f309b0
rhakdnj <chungjm0711@gmail.com>
2023-11-12 10:04:13 +0900

    refactor: 02-1 (#11)

--- refacotring-javascript/chapter11/src/02-1.ts
+++ refacotring-javascript/chapter11/src/02-1.ts
@@ -1,6 +1,11 @@
 const tenPercentRaise = (person: any) => {
-    person.salary = person.salary.multiply(1.1)
-}
+    person.salary = person.salary.multiply(1.1);
+};
 const fivePercentRaise = (person: any) => {
-    person.salary = person.salary.multiply(1.05)
-}
+    person.salary = person.salary.multiply(1.05);
+};
+
+const raise = (person: any, factor: number) => {
+    person.salary = person.salary.multiply(1 + factor);
+};
+
